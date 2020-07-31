e6a346f2161c3ee4341af0edf77974fc89a23c44
W. Nicholas Greene <wng@csail.mit.edu>
2020-07-31 01:40:42 -0400

    Train gta for 15 epochs.

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # GTA-SfM
-python train.py ~/Projects/data/gta_sfm --log-output -b 1 --mindepth 0.1 --nlabel 64 --normalize_depths --stereo_dataset
+python train.py ~/Projects/data/gta_sfm --log-output -b 1 --mindepth 0.1 --nlabel 64 --normalize_depths --stereo_dataset --epochs 15
 
 # DeMoN data
 # python train.py ./dataset/train --log-output -b 1 --mindepth 0.5 --nlabel 64
