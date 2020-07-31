dc86b21e02a980a776d44f80e80a10ad8e112172
W. Nicholas Greene <wng@csail.mit.edu>
2020-07-31 00:56:55 -0400

    Update train.sh.

--- train.sh
+++ train.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-python train.py ~/Projects/data/gta_sfm --log-output -b 1 --mindepth 0.1 --nlabel 64 --normalize_depths
+# GTA-SfM
+python train.py ~/Projects/data/gta_sfm --log-output -b 1 --mindepth 0.1 --nlabel 64 --normalize_depths --stereo_dataset
+
+# DeMoN data
+# python train.py ./dataset/train --log-output -b 1 --mindepth 0.5 --nlabel 64
